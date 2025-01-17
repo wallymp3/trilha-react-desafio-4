767021e1a8a2703c6997d0459eb8e1b6978220d7
Wallace <wall_h@hotmail.com>
2025-01-17 13:49:23 -0300

    adicionando comando para mudar botão

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,19 @@
 import styled from 'styled-components';
 
 
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<{ disabled: boolean }>`
     width: 100%;
     height: 42px;
-    background-color: #81259D;
     color: #FFF;
-
     border: 1px solid #81259D;
     border-radius: 21px;
+    background-color: ${(props) => (props.disabled ? "#cccccc" : "#81259D")};
+    color: ${(props) => (props.disabled ? "#666666" : "white")};
 
     &:hover {
         opacity: 0.6;
         cursor:pointer;
     }
+
+    
 `
